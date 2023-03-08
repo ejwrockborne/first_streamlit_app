b8639f52c2bd36c171670fb131414fa9cb5c833e
ejwrockborne <127319041+ejwrockborne@users.noreply.github.com>
2023-03-08 14:30:20 +0000

    Update streamlit_app.py
    added spaces after emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("🥑Breakfast Menu🍞")
 streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
-streamlit.text("🥗Kale, Spinach and Rocket Smoothie")
-streamlit.text("🐔Hard-boiled Free-Range Egg")
+streamlit.text("🥗 Kale, Spinach and Rocket Smoothie")
+streamlit.text("🐔 Hard-boiled Free-Range Egg")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
