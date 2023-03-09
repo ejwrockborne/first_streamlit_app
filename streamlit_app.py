80d1b61a3b451aefe2ae71f7d871648c1e3e59c2
ejwrockborne <127319041+ejwrockborne@users.noreply.github.com>
2023-03-09 10:54:39 +0000

    Update streamlit_app.py
    added json importing and displaying as a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,9 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
+
+# makes the json look nice 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display the data in a dataframe
+streamlit.dataframe(fruityvice_normalized)
