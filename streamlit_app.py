f0c57fdbfe727ba400242be2e394a123d9f35966
ejwrockborne <127319041+ejwrockborne@users.noreply.github.com>
2023-03-09 15:56:27 +0000

    Update streamlit_app.py
    added try except

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,18 +20,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+  else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
 
+except URLError as e:
+    streamlit.error()
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# makes the json look nice 
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# display the data in a dataframe
-streamlit.dataframe(fruityvice_normalized)
-
-streamlit.stop()
+#streamlit.stop()
 
 
 
